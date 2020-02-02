c344f1de1785aeb15f1cdbe8d51c4701deca60ec
Vincent Wong <TheBrutux168@gmail.com>
2020-02-02 17:35:43 +1100

    Add Stack ADT

---
+++ Stack/stack.py
@@ -0,0 +1,17 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return bool(self.items)
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop()
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
